6084d15569b5a394aa30cbac426badb52d9ba6ce
alexc <alexchb.makarenko@yandex.ru>
2022-08-24 11:02:22 +0300

    first

---
+++ untitled/src/main/java/Radio.java
@@ -0,0 +1,84 @@
+public class Radio {
+
+    private int minNumberRadio = 0;
+    private int maxNumberRadio;
+    private int numberRadio;
+    private int minCurrentVolume = 0;
+    private int maxCurrentVolume = 100;
+   private int currentVolume;
+
+   public Radio(){
+       maxNumberRadio = 9;
+   }
+
+   public Radio(int size){
+       maxNumberRadio = minNumberRadio + size - 1;
+   }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int getMinNumberRadio(){
+        return minNumberRadio;
+    }
+
+    public int getMaxNumberRadio(){
+        return maxNumberRadio;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) {
+        if (newCurrentVolume < minCurrentVolume) {
+            newCurrentVolume = minCurrentVolume;
+        }
+        if (newCurrentVolume > maxCurrentVolume) {
+            newCurrentVolume = maxCurrentVolume;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {
+        if (currentVolume < maxCurrentVolume) {
+            int target = currentVolume + 1;
+            setCurrentVolume(target);
+        }
+    }
+
+    public void decreaseVolume() {
+        if (currentVolume > minCurrentVolume) {
+            int target = currentVolume - 1;
+            setCurrentVolume(target);
+        }
+    }
+
+    public int getNumberRadio() {
+        return numberRadio;
+    }
+
+    public void setNumberRadio(int newNumberRadio) {
+        if (newNumberRadio < 0) {
+            newNumberRadio = -1;
+        }
+        if (newNumberRadio > maxNumberRadio) {
+            newNumberRadio = maxNumberRadio;
+        }
+        numberRadio = newNumberRadio;
+    }
+
+    public void nextRadio() {
+        if (numberRadio < maxNumberRadio) {
+            int target = numberRadio + 1;
+            setNumberRadio(target);
+        }
+    }
+
+    public void prevRadio() {
+        if (numberRadio > 0) {
+            int target = numberRadio - 1;
+            setNumberRadio(target);
+        }
+        if (numberRadio == 0) {
+            numberRadio = maxNumberRadio;
+        }
+    }
+}
---
+++ untitled/src/test/java/TestRadio.java
@@ -0,0 +1,113 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestRadio {
+
+    @Test
+    public void getIncreaseVolume() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(9);
+
+        cond.increaseVolume();
+
+        int expected = 10;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIncreaseVolumeBorder() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(100);
+
+        cond.increaseVolume();
+
+        int expected = 100;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDecreaseVolume() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(1);
+
+        cond.decreaseVolume();
+
+        int expected = 0;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDecreaseVolumeBorder() {
+        Radio cond = new Radio();
+        cond.setCurrentVolume(0);
+
+        cond.decreaseVolume();
+
+        int expected = 0;
+        int actual = cond.getCurrentVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIncreaseRadio() {
+        Radio cond = new Radio(100);
+        cond.setNumberRadio(23);
+
+        cond.nextRadio();
+
+        int expected = 24;
+        int actual = cond.getNumberRadio();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIncreaseRadioBorder() {
+        Radio cond = new Radio(100);
+        cond.setNumberRadio(-9);
+
+        cond.nextRadio();
+
+        int expected = 0;
+        int actual = cond.getNumberRadio();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDecreaseRadio() {
+        Radio cond = new Radio(100);
+        cond.setNumberRadio(50);
+
+        cond.prevRadio();
+
+        int expected = 49;
+        int actual = cond.getNumberRadio();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getDecreaseRadioBorder() {
+        Radio cond = new Radio(100);
+        cond.setNumberRadio(0);
+
+        cond.prevRadio();
+
+        int expected = 99;
+        int actual = cond.getNumberRadio();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void test(){
+        Radio cond = new Radio ();
+        cond.setNumberRadio(5);
+
+        Assertions.assertEquals(0,cond.getMinNumberRadio());
+        Assertions.assertEquals(9,cond.getMaxNumberRadio());
+        Assertions.assertEquals(5,cond.getNumberRadio());
+    }
+}
